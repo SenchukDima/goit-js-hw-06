bd3cbf74932cd3633d87c5649a63369bff06f9cf
unknown <senchuk1205@gmail.com>
2019-11-10 14:35:00 +0200

    add_module-6_hw_v2

--- js/task-7.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import users from './users.js';
-
-const calculateTotalBalance = users => {
-  return users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
-};
-
-console.log(calculateTotalBalance(users)); // 20916
